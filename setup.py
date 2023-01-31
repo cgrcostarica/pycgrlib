fb405815198d62cadb02436f4318084d9f74731f
Mario Zamora <mortasoft@gmail.com>
2023-01-30 18:35:48 -0600

    Update setup.py
    Update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pycgrlib',
-    version='0.1.1',
+    version='0.1.2',
     url='https://github.com/cgrcr/pycgrlib.git',
     description='Libreria de funciones para uso de herramientas en la Contraloria General de la Republica (CGR)',
     author='Mario Zamora Madriz',
